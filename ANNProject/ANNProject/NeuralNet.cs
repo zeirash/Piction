47fe12eaf3708562d66670820155f5da8b66a1a1
Atn010 <atn010g@gmail.com>
2016-10-16 21:52:21 +0700

    retrying to remake NeuralNet

--- ANNProject/ANNProject/NeuralNet.cs
+++ ANNProject/ANNProject/NeuralNet.cs
@@ -4,43 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
+using System.Drawing;
 using System.IO;
+
 using AForge.Neuro;
 using AForge.Neuro.Learning;
-using System.Drawing;
+using AForge.Imaging.Filters;
 using AForge.Imaging;
+using Accord.Imaging.Converters;
 
 namespace ANNProject
 {
     class NeuralNet
     {
-        //activations
-        ActivationNetwork an;
-        BackPropagationLearning bpl;
-        SOMLearning sml;
 
-
-        public void setActivationNetwork(int inputLayerCount, int hiddenLayerCount, int outputLayerCount)
+        public double [][] imageProcessing(String [] images)
         {
-            an = new ActivationNetwork(new SigmoidFunction(), inputLayerCount, hiddenLayerCount, outputLayerCount);
-        }
+            var imageToArray = new ImageToArray(min: -1, max: +1);
+            var toGrayscale = new Grayscale(0, 0, 0);
+            var goThreshold = new Threshold();
+            var reduceNoise = new AdditiveNoise();
+            var reScaling = new ResizeBicubic(10, 10);
+            double [][] processed = new double[images.Length][];
 
-        public void loadArt(String filePath)
-        {
-
-            Bitmap image = AForge.Imaging.Image.FromFile(filePath);
-
-        }
-        
-        public void runClassificationEpoch()
-        {
-            int epoch = 10000;
-            double error = 0;
-            for (int cycle = 0; cycle < epoch; cycle++)
+            int i = 0;
+            foreach (String file in images)
             {
-                error = an.Compute(0,0);
+                var image = AForge.Imaging.Image.FromFile(file);
+                image = toGrayscale.Apply(image);
+                image = goThreshold.Apply(image);
+                image = reduceNoise.Apply(image);
+                image = reScaling.Apply(image);
+                imageToArray.Convert(image, out processed[i]);
+                i++;
             }
+            return processed;
         }
 
         public void preLoad()
