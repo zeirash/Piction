db2b2d9499448dc5f6646d1b89954d3b09091314
Tiffany <tiff.wijaya@yahoo.com>
2016-11-08 10:12:17 +0700

    small change neuralnet

--- ANNProject/ANNProject/NeuralNet.cs
+++ ANNProject/ANNProject/NeuralNet.cs
@@ -29,8 +29,33 @@ namespace ANNProject
         public List<String> listCategoryNames;
         List<double[]> data_simpanan = new List<double[]>();
         BackPropagationLearning bpnn;
-        List<string> category = new List<string>;
+        //List<string> category = new List<string>;
 
+        /*
+        public double [][] imageProcessing(String [] images)
+        {
+            BackPropagationLearning bpnn;
+            ActivationNetwork an;
+            var imageToArray = new ImageToArray(min: -1, max: +1);
+            var toGrayscale = new Grayscale(0, 0, 0);
+            var goThreshold = new Threshold(128);
+            var reduceNoise = new AdditiveNoise();
+            var reScaling = new ResizeBicubic(10, 10);
+            double [][] processed = new double[images.Length][];
+
+            int i = 0;
+            foreach (String file in images)
+            {
+                var image = AForge.Imaging.Image.FromFile(file);
+                image = toGrayscale.Apply(image);
+                image = goThreshold.Apply(image);
+                image = reduceNoise.Apply(image);
+                image = reScaling.Apply(image);
+                imageToArray.Convert(image, out processed[i]);
+                i++;
+            }
+            return processed;
+        }*/
 
         public Bitmap preprocessing(Bitmap image)
         {
